dfb9be189e080209266c3c2397bbb3e800b2de79
giahuy692 <it.dogiahuy@gmail.com>
2022-05-17 21:35:04 +0700

    Login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,13 @@ const authControllers = require("../controllers/authControllers");
 
 const router = require("express").Router();
 
-
+//Register
 router.post("/register",authControllers.registerUser);
+
+
+//Login
+router.post("/login",authControllers.loginUser);
+
+
 
 module.exports = router;
